57aa7f810c6856c8d75be87814a8b03eacc72713
Erisfashion <erisfashion@gmail.com>
2026-09-16 16:07:03 +0800

    Fix import statement for File in ChooserDialog

--- app/src/main/java/com/obsez/android/lib/filechooser/ChooserDialog.java
+++ app/src/main/java/com/obsez/android/lib/filechooser/ChooserDialog.java
@@ -1,10 +1,15 @@
 package com.obsez.android.lib.filechooser;
+
 import android.app.Activity;
+import java.io.File;
+
 public class ChooserDialog {
     public ChooserDialog(Activity activity) {}
     public ChooserDialog title(CharSequence title) { return this; }
     public ChooserDialog withFilter(boolean allowDir, boolean allowFiles, String... extensions) { return this; }
     public ChooserDialog into(Result callback) { return this; }
     public void show() {}
-    public interface Result { void onChoosePath(String path, android.io.File pathFile); }
+    public interface Result {
+        void onChoosePath(String path, File pathFile);
+    }
 }
