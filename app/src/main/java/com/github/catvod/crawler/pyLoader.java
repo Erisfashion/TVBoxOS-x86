fec694a6de4935975e0aded16586e5ceb04ea463
Erisfashion <erisfashion@gmail.com>
2026-09-16 19:30:53 +0800

    Implement additional methods in pyLoader class

--- app/src/main/java/com/github/catvod/crawler/pyLoader.java
+++ app/src/main/java/com/github/catvod/crawler/pyLoader.java
@@ -1,10 +1,34 @@
 package com.github.catvod.crawler;
 
 import com.github.catvod.crawler.python.IPyLoader;
+import java.util.Map;
 
 public class pyLoader implements IPyLoader {
+
     @Override
-    public Spider getSpider(String key, String api, String ext) {
+    public void clear() {
+    }
+
+    @Override
+    public void setConfig(String jsonStr) {
+    }
+
+    @Override
+    public void setRecentPyKey(String key) {
+    }
+
+    @Override
+    public Spider getSpider(String key, String cls, String ext) {
         return new SpiderNull();
+    }
+
+    @Override
+    public Object[] proxyInvoke(Map<String, String> params) {
+        return null;
+    }
+
+    @Override
+    public Object[] proxyInvoke(Map<String, String> params, String key) {
+        return null;
     }
 }
