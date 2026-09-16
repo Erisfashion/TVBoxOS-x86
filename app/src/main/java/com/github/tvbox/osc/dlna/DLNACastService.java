08bdf2f3d5e48185138c2a8bc295a73e43a2e17d
Erisfashion <erisfashion@gmail.com>
2026-09-16 15:47:19 +0800

    Change DLNACastService to extend Android Service
    Refactor DLNACastService to extend Android Service instead of UpnpServiceImpl.

--- app/src/main/java/com/github/tvbox/osc/dlna/DLNACastService.java
+++ app/src/main/java/com/github/tvbox/osc/dlna/DLNACastService.java
@@ -1,18 +1,12 @@
 package com.github.tvbox.osc.dlna;
 
-import org.fourthline.cling.UpnpServiceConfiguration;
-import org.fourthline.cling.android.AndroidUpnpServiceImpl;
-import org.fourthline.cling.model.types.ServiceType;
-import org.fourthline.cling.model.types.UDAServiceType;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
 
-public class DLNACastService extends AndroidUpnpServiceImpl {
+public class DLNACastService extends Service {
     @Override
-    protected UpnpServiceConfiguration createConfiguration() {
-        return new DLNAServiceConfiguration() {
-            @Override
-            public ServiceType[] getExclusiveServiceTypes() {
-                return new ServiceType[]{new UDAServiceType("AVTransport", 1)};
-            }
-        };
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 }
