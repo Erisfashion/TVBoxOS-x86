485be8293288de1bcd40f29155bfc0e3b24d73a9
Erisfashion <erisfashion@gmail.com>
2026-09-16 13:44:50 +0800

    Add TvRecyclerView class extending RecyclerView

---
+++ tvrecyclerview/src/main/java/com/owen/tvrecyclerview/widget/TvRecyclerView.java
@@ -0,0 +1,12 @@
+package com.owen.tvrecyclerview.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TvRecyclerView extends RecyclerView {
+    public TvRecyclerView(Context context) { super(context); }
+    public TvRecyclerView(Context context, @Nullable AttributeSet attrs) { super(context, attrs); }
+    public TvRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) { super(context, attrs, defStyle); }
+}
