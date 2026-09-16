aa30f5ef22fd9c6f5ea9f4442a26272cf0e7921c
Erisfashion <erisfashion@gmail.com>
2026-09-16 10:37:26 +0800

    Refactor App class to extend Application
    Refactor App class to extend Application and initialize components.

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -1,117 +1,55 @@
 package com.github.tvbox.osc.base;
 
-import android.app.Activity;
-import androidx.multidex.MultiDexApplication;
-
-import com.github.tvbox.osc.bean.VodInfo;
+import android.app.Application;
+import android.content.Context;
+import androidx.multidex.MultiDex;
+import com.github.tvbox.osc.cache.RoomDataManger;
 import com.github.tvbox.osc.callback.EmptyCallback;
 import com.github.tvbox.osc.callback.LoadingCallback;
-import com.github.tvbox.osc.data.AppDataManager;
 import com.github.tvbox.osc.server.ControlManager;
-import com.github.tvbox.osc.util.AppManager;
-import com.github.tvbox.osc.util.EpgUtil;
-import com.github.tvbox.osc.util.FileUtils;
-import com.github.tvbox.osc.util.HawkConfig;
-import com.github.tvbox.osc.util.LOG;
-import com.github.tvbox.osc.util.OkGoHelper;
-import com.github.tvbox.osc.util.PlayerHelper;
 import com.kingja.loadsir.core.LoadSir;
 import com.orhanobut.hawk.Hawk;
-import com.p2p.P2PClass;
-import com.whl.quickjs.android.QuickJSLoader;
-import com.github.catvod.crawler.JsLoader;
+import java.security.Security;
+import org.conscrypt.Conscrypt;
 
-import me.jessyan.autosize.AutoSizeConfig;
-import me.jessyan.autosize.unit.Subunits;
+public class App extends Application {
 
-/**
- * @author pj567
- * @date :2020/12/17
- * @description:
- */
-public class App extends MultiDexApplication {
     private static App instance;
-
-    private static P2PClass p;
-    public static String burl;
-    private static String dashData;
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        instance = this;
-        initParams();
-        // OKGo
-        OkGoHelper.init(); //台标获取
-        EpgUtil.init();
-        // 初始化Web服务器
-        ControlManager.init(this);
-        //初始化数据库
-        AppDataManager.init();
-        LoadSir.beginBuilder()
-                .addCallback(new EmptyCallback())
-                .addCallback(new LoadingCallback())
-                .commit();
-        AutoSizeConfig.getInstance().setCustomFragment(true).getUnitsManager()
-                .setSupportDP(false)
-                .setSupportSP(false)
-                .setSupportSubunits(Subunits.MM);
-        PlayerHelper.init();
-        QuickJSLoader.init();
-        FileUtils.cleanPlayerCache();
-    }
-
-    private void initParams() {
-        // Hawk
-        Hawk.init(this).build();
-        Hawk.put(HawkConfig.DEBUG_OPEN, false);
-        Hawk.put(HawkConfig.PLAYER_IS_LIVE, false);
-        if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
-            Hawk.put(HawkConfig.PLAY_TYPE, 2);
-        } else if (Hawk.get(HawkConfig.PLAY_TYPE, 2) == 0) {
-            Hawk.put(HawkConfig.PLAY_TYPE, 2);
-        }
-    }
 
     public static App getInstance() {
         return instance;
     }
 
     @Override
-    public void onTerminate() {
-        super.onTerminate();
-        JsLoader.destroy();
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        // Android 4.2.2 (Dalvik) 必须先加载分包，否则后续代码报 NoClassDefFoundError
+        MultiDex.install(this);
     }
 
-
-    private VodInfo vodInfo;
-    public void setVodInfo(VodInfo vodinfo){
-        this.vodInfo = vodinfo;
-    }
-    public VodInfo getVodInfo(){
-        return this.vodInfo;
-    }
-
-    public static P2PClass getp2p() {
+    @Override
+    public void onCreate() {
+        // 关键：在任何网络操作前插入 Conscrypt，使 Android 4.2.2 原生支持现代 TLS 1.2+
         try {
-            if (p == null) {
-                p = new P2PClass(FileUtils.getExternalCachePath());
-            }
-            return p;
-        } catch (Exception e) {
-            LOG.e(e.toString());
-            return null;
+            Security.insertProviderAt(Conscrypt.newProvider(), 1);
+        } catch (Throwable t) {
+            t.printStackTrace();
         }
-    }
 
-    public Activity getCurrentActivity() {
-        return AppManager.getInstance().currentActivity();
-    }
+        super.onCreate();
+        instance = this;
 
-    public void setDashData(String data) {
-        dashData = data;
-    }
-    public String getDashData() {
-        return dashData;
+        // 初始化本地轻量存储
+        Hawk.init(this).build();
+
+        // 初始化数据库与后台服务控制组件
+        RoomDataManger.init(this);
+        ControlManager.init(this);
+
+        // 初始化状态页加载器
+        LoadSir.beginBuilder()
+                .addCallback(new EmptyCallback())
+                .addCallback(new LoadingCallback())
+                .commit();
     }
 }
