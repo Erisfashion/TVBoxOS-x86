a8c8cfeff673488cd56c6baff7ce4ad3c8a5d963
Erisfashion <erisfashion@gmail.com>
2026-09-16 13:45:15 +0800

    Add V7GridLayoutManager class extending GridLayoutManager

---
+++ tvrecyclerview/src/main/java/com/owen/tvrecyclerview/widget/V7GridLayoutManager.java
@@ -0,0 +1,9 @@
+package com.owen.tvrecyclerview.widget;
+
+import android.content.Context;
+import androidx.recyclerview.widget.GridLayoutManager;
+
+public class V7GridLayoutManager extends GridLayoutManager {
+    public V7GridLayoutManager(Context context, int spanCount) { super(context, spanCount); }
+    public V7GridLayoutManager(Context context, int spanCount, int orientation, boolean reverseLayout) { super(context, spanCount, orientation, reverseLayout); }
+}
