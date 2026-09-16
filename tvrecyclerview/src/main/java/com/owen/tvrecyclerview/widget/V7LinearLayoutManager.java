537b0177c71977c7560d837881584a8dcf6b91c9
Erisfashion <erisfashion@gmail.com>
2026-09-16 13:45:48 +0800

    Add V7LinearLayoutManager class for RecyclerView

---
+++ tvrecyclerview/src/main/java/com/owen/tvrecyclerview/widget/V7LinearLayoutManager.java
@@ -0,0 +1,9 @@
+package com.owen.tvrecyclerview.widget;
+
+import android.content.Context;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+public class V7LinearLayoutManager extends LinearLayoutManager {
+    public V7LinearLayoutManager(Context context) { super(context); }
+    public V7LinearLayoutManager(Context context, int orientation, boolean reverseLayout) { super(context, orientation, reverseLayout); }
+}
