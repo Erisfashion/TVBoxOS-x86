793a6f69d873d0677d6a71a6db19236dad220312
Erisfashion <erisfashion@gmail.com>
2026-09-17 07:02:26 +0800

    Enhance App class with new properties and methods
    Added methods to manage dashData, currentActivity, and vodInfo.

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -1,17 +1,22 @@
 package com.github.tvbox.osc.base;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
+import android.os.Bundle;
 import androidx.multidex.MultiDex;
+import com.github.tvbox.osc.bean.VodInfo;
 
 public class App extends Application {
 
     private static App instance;
+    private String dashData;
+    private Activity currentActivity;
+    private VodInfo vodInfo;
 
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
-        // Android 4.2.2 启动必备，必须在 super 之后立即注入
         MultiDex.install(this);
     }
 
@@ -19,9 +24,61 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {}
+
+            @Override
+            public void onActivityStarted(Activity activity) {}
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+                currentActivity = activity;
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {}
+
+            @Override
+            public void onActivityStopped(Activity activity) {}
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {}
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+                if (currentActivity == activity) {
+                    currentActivity = null;
+                }
+            }
+        });
     }
 
     public static App getInstance() {
         return instance;
+    }
+
+    public String getDashData() {
+        return dashData;
+    }
+
+    public void setDashData(String dashData) {
+        this.dashData = dashData;
+    }
+
+    public Activity getCurrentActivity() {
+        return currentActivity;
+    }
+
+    public void setCurrentActivity(Activity currentActivity) {
+        this.currentActivity = currentActivity;
+    }
+
+    public VodInfo getVodInfo() {
+        return vodInfo;
+    }
+
+    public void setVodInfo(VodInfo vodInfo) {
+        this.vodInfo = vodInfo;
     }
 }
