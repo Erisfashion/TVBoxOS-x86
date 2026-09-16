c2ddd13d483de04afbaaa95c7e0f5894f981944c
Erisfashion <erisfashion@gmail.com>
2026-09-16 15:59:00 +0800

    Update GridLayoutManager.java

--- tvrecyclerview/src/main/java/com/owen/tvrecyclerview/widget/GridLayoutManager.java
+++ tvrecyclerview/src/main/java/com/owen/tvrecyclerview/widget/GridLayoutManager.java
@@ -1,10 +1,13 @@
 package com.owen.tvrecyclerview.widget;
 
 import android.content.Context;
-import androidx.recyclerview.widget.GridLayoutManager;
 
 public class GridLayoutManager extends androidx.recyclerview.widget.GridLayoutManager {
     public GridLayoutManager(Context context, int spanCount) {
         super(context, spanCount);
+    }
+
+    public GridLayoutManager(Context context, int spanCount, int orientation, boolean reverseLayout) {
+        super(context, spanCount, orientation, reverseLayout);
     }
 }
